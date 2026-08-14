2d9aab8c942fbdb7109a93be65cad8dbb01cf753
Kamil Mrzygłód <kamil@thecloudtheory.com>
2026-08-14 08:30:45 +0200

    Enhance `TestConnectionAsync` in `NeyrdSender`: add retries with exponential backoff, detailed logging, and refined exception handling for connection attempts.

--- neyrd.emitter/Networking/NeyrdSender.cs
+++ neyrd.emitter/Networking/NeyrdSender.cs
@@ -11,7 +11,18 @@ internal sealed class NeyrdSender(IPAddress emitterIpAddress) : IDisposable
     private readonly Socket _socket = new(SocketType.Stream,
         ProtocolType.Tcp);
 
-    public async Task<ConnectionTestResult> TestConnectionAsync()
+    /// <summary>
+    /// Tests the connection to the listener by sending a series of messages and ensures the communication is successful.
+    /// </summary>
+    /// <param name="attempt">
+    /// The current attempt number for establishing the connection. Defaults to 1.
+    /// The method retries up to 10 times if the connection fails.
+    /// </param>
+    /// <returns>
+    /// A <see cref="ConnectionTestResult"/> object encapsulating the results of the connection test.
+    /// Contains information about the success or failure of the operation, including any error messages or exceptions.
+    /// </returns>
+    public async Task<ConnectionTestResult> TestConnectionAsync(int attempt = 1)
     {
         try
         {
@@ -38,11 +49,21 @@ internal sealed class NeyrdSender(IPAddress emitterIpAddress) : IDisposable
         }
         catch (SocketException ex)
         {
-            return new ConnectionTestResult
+            if (attempt == 10)
             {
-                ErrorMessage = "The connection could not be established.",
-                Exception = ex
-            };
+                return new ConnectionTestResult
+                {
+                    ErrorMessage = "The connection could not be established.",
+                    Exception = ex
+                };
+            }
+
+            attempt++;
+            var nextDelay = 1000 * attempt;
+            NeyrdLogger.Log($"Attempt {attempt}/10 to connect. Failed: {ex.Message}. Retrying after {nextDelay}ms.");
+
+            await Task.Delay(nextDelay);
+            await TestConnectionAsync(attempt);
         }
 
         return new ConnectionTestResult
