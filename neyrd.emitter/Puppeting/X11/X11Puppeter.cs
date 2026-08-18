ce6753eef9994f3373744950e295f10e43021b31
Kamil Mrzygłód <kamil@thecloudtheory.com>
2026-08-18 18:34:03 +0200

    Add delay before mouse button release event for improved click handling

--- neyrd.emitter/Puppeting/X11/X11Puppeter.cs
+++ neyrd.emitter/Puppeting/X11/X11Puppeter.cs
@@ -59,7 +59,9 @@ internal sealed partial class X11Puppeter : IPuppeter
             NeyrdLogger.Log($"Display: {_display != IntPtr.Zero}");
             
             _ = XTestFakeButtonEvent(_display, (uint)button, true, 0);
-            _ = XTestFakeButtonEvent(_display, (uint)button, false, 50);
+            _ = XFlush(_display);
+            Thread.Sleep(50);
+            _ = XTestFakeButtonEvent(_display, (uint)button, false, 0);
             _ = XFlush(_display);
         }
         catch (DllNotFoundException ex)
