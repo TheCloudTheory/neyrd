542b31369a4126e82d2b4ef77374e899f115e3c7
kamil-mrzyglod <kamil@thecloudtheory.com>
2026-08-20 17:21:16 +0200

    Refactor NeyrdLogger to implement log file rotation and improve logging mechanism

--- neyrd.core/NeyrdLogger.cs
+++ neyrd.core/NeyrdLogger.cs
@@ -2,13 +2,30 @@ namespace neyrd.core;
 
 public static class NeyrdLogger
 {
+    private const int MaxLogFiles = 10;
+    private const int MaxLinesPerLogFile = 10000;
+
+    private static readonly Lock Lock = new();
+    private static int _currentLogLine;
+    private static int _currentLogFileIndex;
+    
     static NeyrdLogger()
     {
-        File.WriteAllText("neyrd.log", string.Empty);    
+        File.WriteAllText("neyrd.0.log", string.Empty);    
     }
     
     public static void Log(string message)
     {
-        File.AppendAllText("neyrd.log", $"[{DateTimeOffset.Now}] {message}{System.Environment.NewLine}");
+        lock (Lock)
+        {
+            File.AppendAllText($"neyrd.{_currentLogFileIndex}.log", $"[{DateTimeOffset.Now}] {message}{System.Environment.NewLine}");
+            _currentLogLine++;
+
+            if (_currentLogLine < MaxLinesPerLogFile) return;
+            
+            _currentLogLine = 0;
+            _currentLogFileIndex = (_currentLogFileIndex + 1) % MaxLogFiles;
+            File.WriteAllText($"neyrd.{_currentLogFileIndex}.log", string.Empty);
+        }
     }
 }
